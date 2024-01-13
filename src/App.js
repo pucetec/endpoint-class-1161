6937c6989a0898660e24655a6ed9162705faa48d
Diego-Bears <quilachamind@gmail.com>
2024-01-13 13:44:36 -0500

    Feat:[no story][Diego Quilachamin] practica endpoint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+
+    const fetchUsers = async () => {
+      try {
+        const tempUsers = await axios.get("https://jsonplaceholder.typicode.com/users");
+        setUsers(tempUsers.data);
+      } catch (error) {
+        console.error("No se encontró ningún usuario:", error);
+      }
+    }
+    fetchUsers();
+  }, [users]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <h1 className="display-4 fw-semibold text-dark text-center my-5">
+        <center>Lista de Usuario</center>  
+      </h1>
+      <table className="table table-bordered mt-5">
+        <thead className="table-dark">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">User</th>
+            <th scope="col">Email</th>
+            <th scope="col">Phone</th>
+            <th scope="col">Website</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => (
+            <tr key={index}>
+              <th scope="row">{user.id}</th>
+              <td>{user.name}</td>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+              <td>{user.phone}</td>
+              <td>{user.website}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
