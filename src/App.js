dd975314e4a32b274eb8620f0b556b2cf0232ede
Kevin C <kevinrcg09@gmail.com>
2024-01-13 13:13:39 -0500

    Feat:[KevinC-NOSTORY][KEVIN]Creating an endpoint data for a table.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,61 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from "axios";
 
 function App() {
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      name: "kevin",
+      username: "ilinkzzx",
+      email: "ilinkzzx@gmail.com",
+      phone: "0992883356"
+    }
+  ]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const tempUsers = await axios.get("https://jsonplaceholder.typicode.com/users");
+      setUsers(tempUsers.data);
+    };
+    fetchUsers();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Lista de contacto
+        <table>
+          <thead>
+            <tr>
+              <th>
+                #
+              </th>
+              <th>
+                Nombre
+              </th>
+              <th>
+                Username
+              </th>
+              <th>
+                Email
+              </th>
+              <th>
+                Telefono
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) =>
+              <tr key={index}>
+                <td >{user.id}</td>
+                <td >{user.name}</td>
+                <td >{user.username}</td>
+                <td >{user.email}</td>
+                <td >{user.phone}</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
       </header>
     </div>
   );
