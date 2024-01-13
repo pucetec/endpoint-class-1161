57b5013fc79671b0518a7685e6ccf29266632619
Cross2203 <ciae1996@gmail.com>
2024-01-13 13:08:59 -0500

    feat: [nostory][Cristian] added table and conection with endpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      name: "David",
+      username: "David123",
+      email: "david@david",
+      phone: "098898124",
+      website: "www.david.com",
+    },
+    {
+      id: 2,
+      name: "Joseph",
+      username: "joseph123",
+      email: "joseph@joseph",
+      phone: "095646524",
+      website: "joseph.com",
+    },
+    {
+      id: 3,
+      name: "Mark",
+      username: "Mark123",
+      email: "mark@mark",
+      phone: "094656604",
+      website: "mark.com",
+    },
+  ]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const finalUsers = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      setUsers(finalUsers.data);
+    };
+    fetchUsers();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Lista de usuarios</h1>
       </header>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Nombre</th>
+            <th>Nombre de usuario</th>
+            <th>E-mail</th>
+            <th>telefono</th>
+            <th>Sitio Web</th>
+          </tr>
+        </thead>
+        {users.map((user, index) => (
+          <tbody>
+            <tr>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+              <td>{user.phone}</td>
+              <td>{user.website}</td>
+            </tr>
+          </tbody>
+        ))}
+      </table>
     </div>
   );
-}
+};
 
 export default App;
