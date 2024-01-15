a8cfdefeae03d13858cba245ea322a8508af63d5
Favofrito3 <buitronkevin2010@gmail.com>
2024-01-14 20:47:25 -0500

    Feat: [Nostory][Fabian Buitron] Iimplementacion de usuario

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [users, setUsers] = useState([{
+    id: 1,
+    name: "John",
+    username: "John123",
+    email: "john@john",
+    phone: "0999999999",
+    website: "john.com",
+  },
+  {
+    id: 2,
+    name: "Jane",
+    username: "Jane123",
+    email: "jane@jane",
+    phone: "0999999999",
+    website: "jane.com",
+  },
+  {
+    id: 3,
+    name: "Mark",
+    username: "Mark123",
+    email: "mark@mark",
+    phone: "0999999999",
+    website: "mark.com",
+  },]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+
+      const tempUsers = await axios.get("https://jsonplaceholder.typicode.com/users");
+      setUsers(tempUsers.data);
+    };
+
+    fetchUsers();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <h1>
+        Lista de Usuarios
+      </h1>
+      <table className=" ">
+        <thead className=" ">
+          <tr>
+            <th >#</th>
+            <th >Nombre</th>
+            <th >Usuario</th>
+            <th >Correo electrónico</th>
+            <th >Teléfono</th>
+            <th >Sitio web</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <th scope="row">{user.id}</th>
+              <td>{user.name}</td>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+              <td>{user.phone}</td>
+              <td>{user.website}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
